9aa671a27548933ce7c2ae8f14056a8312b28387
abhijeet831 <abhijeet.ssingh.1490@gmail.com>
2026-01-28 13:29:06 +0530

    Initial commit

---
+++ readme.py
@@ -0,0 +1,27 @@
+"""
+Day 1 Task :
+
+1. installed vs code and python install manager
+
+2. work with pip library manager  -> 
+
+    version check->
+    
+    python -m pip --version 
+    installed couple of libraries-> numpy and pandas
+
+    i. pip install numpy
+    ii.pip install pandas
+
+3. virtual env -> to install all dependencies (eg. python libraries) at the same folder
+
+
+    to create -> python -m venv venv
+        python -m venv(name) venv(path)
+    to activate-> .\venv\Scripts\Activate.ps1
+
+4. git basics.
+
+
+
+"""
