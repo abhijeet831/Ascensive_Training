a62e5ca1681ea1ea29537153f6cc7855708d9f88
abhijeet831 <abhijeet.ssingh.1490@gmail.com>
2026-01-29 10:41:35 +0530

    Day2 changes

--- readme.py
+++ readme.py
@@ -12,6 +12,26 @@ Day 1 Task :
 
     i. pip install numpy
     ii.pip install pandas
+    iii. pip uninstall numpy
+    iv. pip show pandas
+    v. pip list
+
+    install all dependengitcies from requirements.txt file ->
+    pip install -r requirements.txt
+
+    pip freeeze 
+    The Python pip freeze command is used to list packages that don't come pre-installed with Python. 
+
+            Syntax: 
+
+            pip freeze
+    
+    Python pip list --outdated command is used to list all the packages that are outdated. This command cross-checks the installed package information with the PIP repository.
+
+            Syntax:
+
+            pip list --outdated
+
 
 3. virtual env -> to install all dependencies (eg. python libraries) at the same folder
 
@@ -21,7 +41,6 @@ Day 1 Task :
     to activate-> .\venv\Scripts\Activate.ps1
 
 4. git basics.
-
 
 
 """
